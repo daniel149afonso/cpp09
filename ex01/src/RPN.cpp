c4645eaec093770d5ee52b75af6014945e71c3ab
Daniel Afonso <daniel149afonso@users.noreply.github.com>
2026-05-06 18:12:09 +0200

    mergeSort in progres

--- ex01/src/RPN.cpp
+++ ex01/src/RPN.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   RPN.cpp                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: daniel <daniel@student.42.fr>              +#+  +:+       +#+        */
+/*   By: daafonso <daafonso@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/04 14:05:59 by daniel            #+#    #+#             */
-/*   Updated: 2026/05/06 15:33:23 by daniel           ###   ########.fr       */
+/*   Updated: 2026/05/06 16:04:39 by daafonso         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,50 +14,50 @@
 
 // check operator
 int applyOp(int a, int b, char op) {
-    if (op == '+') return a + b;
-    if (op == '-') return a - b;
-    if (op == '*') return a * b;
-    if (op == '/') {
-        if (b == 0) throw std::runtime_error("Error"); //divid by 0
-        return a / b;
-    }
-    throw std::runtime_error("Error");
+	if (op == '+') return a + b;
+	if (op == '-') return a - b;
+	if (op == '*') return a * b;
+	if (op == '/') {
+		if (b == 0) throw std::runtime_error("Error"); //divid by 0
+		return a / b;
+	}
+	throw std::runtime_error("Error");
 }
 
 int rpn(char **argv) {
-    std::stringstream ss(argv[1]);
-    std::string input;
-    std::stack<int> st;
+	std::stringstream ss(argv[1]);
+	std::string input;
+	std::stack<int> st;
 
-    while (ss >> input) {
-        if (input.length() == 1 && std::isdigit(input[0])) { // cas number
-            st.push(input[0] - '0'); // char -> int conversion
-        }
-        else if (input.length() == 1 && //cas operator
-        (input[0] == '+' || input[0] == '-' ||
-         input[0] == '*' || input[0] == '/')) {
-            if (st.size() < 2)
-                return std::cerr << "Error\n", 1;
-            int op = input[0];
-            int b = st.top();
-            st.pop();
+	while (ss >> input) {
+		if (input.length() == 1 && std::isdigit(input[0])) { // cas number
+			st.push(input[0] - '0'); // char -> int conversion
+		}
+		else if (input.length() == 1 && // cas operator
+		(input[0] == '+' || input[0] == '-' ||
+		 input[0] == '*' || input[0] == '/')) {
+			if (st.size() < 2)
+				return std::cerr << "Error\n", 1;
+			int op = input[0];
+			int b = st.top();
+			st.pop();
 
-            int a = st.top();
-            st.pop();
-            try {
-                int result = applyOp(a, b, op);
-                st.push(result);
-            }
-            catch(const std::exception& e) {
-                return std::cerr << e.what() << '\n', 1;
-            }
-        }
-        else // invalid token
-            return std::cerr << "Error\n", 1;
-    }
-    if (st.size() != 1)
-        return std::cerr << "Error\n", 1;
+			int a = st.top();
+			st.pop();
+			try {
+				int result = applyOp(a, b, op);
+				st.push(result);
+			}
+			catch(const std::exception& e) {
+				return std::cerr << e.what() << '\n', 1;
+			}
+		}
+		else // invalid token
+			return std::cerr << "Error\n", 1;
+	}
+	if (st.size() != 1)
+		return std::cerr << "Error\n", 1;
 
-    std::cout << st.top() << std::endl;
-    return 0;
+	std::cout << st.top() << std::endl;
+	return 0;
 }
--- ex02/includes/PmergeMe.hpp
+++ ex02/includes/PmergeMe.hpp
@@ -3,19 +3,24 @@
 /*                                                        :::      ::::::::   */
 /*   PmergeMe.hpp                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: daniel <daniel@student.42.fr>              +#+  +:+       +#+        */
+/*   By: daafonso <daafonso@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/05 16:44:01 by daafonso          #+#    #+#             */
-/*   Updated: 2026/05/05 22:23:49 by daniel           ###   ########.fr       */
+/*   Updated: 2026/05/06 18:09:08 by daafonso         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #pragma once
+#include <iostream>
+#include <cstdlib>
+#include <cctype>
 #include <vector>
-#include <deque>
+#include <sstream>
 
 class PmergeMe {
 public:
     static void sortVector(std::vector<int>& v);
     static void sortDeque(std::deque<int>& d);
 };
+
+int mergeSort(char **argv, int argc);
